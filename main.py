abcbd486206c7ab90ff21e471686db34aacb7f88
Winmarv <winniengaji@yahoo.com>
2023-08-27 16:18:30 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+    print('Hello, World!')
+if __name__ == '__main__':
+    main()
+# Hello, world!
+
+from Library import greet
+def main ():
+    greet()
+if __name__ == '__main__':
+    main()
+# Hello, world!
